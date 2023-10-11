ec5927bd9531dd5f7e9944f21b27dc6e55f7e06b
ramu <vits.18731a0539@gmail.com>
2023-10-11 15:01:21 -0500

    Connected to DB

---
+++ BingoWebApp/BingoWebApp/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BingoWebApp.Entities
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            Deliveries = new HashSet<Delivery>();
+            OrderItems = new HashSet<OrderItem>();
+            Payments = new HashSet<Payment>();
+        }
+
+        public int OrderId { get; set; }
+        public int? UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int Status { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public virtual User? User { get; set; }
+        public virtual ICollection<Delivery> Deliveries { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+        public virtual ICollection<Payment> Payments { get; set; }
+    }
+}
---
+++ BingoWebApp/BingoWebApp/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BingoWebApp.Entities
+{
+    public partial class User
+    {
+        public User()
+        {
+            Deliveries = new HashSet<Delivery>();
+            DriverLocations = new HashSet<DriverLocation>();
+            Orders = new HashSet<Order>();
+        }
+
+        public int UserId { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public int Role { get; set; }
+
+        public virtual ICollection<Delivery> Deliveries { get; set; }
+        public virtual ICollection<DriverLocation> DriverLocations { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
